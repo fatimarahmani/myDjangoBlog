2372293297fe7a6989c872c43ec7bb6a788ab12c
fatimarahmani <f.rahmani99@gmail.com>
2022-08-27 15:24:58 +0430

    make details of slug

--- articles/views.py
+++ articles/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render,HttpResponse
 from . import models
 
 def maqalat(request):
-    x = models.Articles.objects.all().order_by('date')
+    x = models.Articles.objects.all().order_by('-date')
     args = {'maqalat':x}
     return render(request, 'maqalat.html', args)
 
 def showSlug(request, slug):
-    return HttpResponse(slug)
+    y = models.Articles.objects.get(slug = slug)
+    args = {'details':y}
+    return render(request, 'details.html',args)
